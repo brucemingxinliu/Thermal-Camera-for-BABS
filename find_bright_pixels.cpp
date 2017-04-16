fa0056defd49f79756e566994a310b5827c98732
brucemingxinliu <mxl592@case.edu>
2017-04-16 00:50:28 -0400

    Create find_bright_pixels.cpp

---
+++ find_bright_pixels.cpp
@@ -0,0 +1,125 @@
+//created by Mingxin Liu
+//maintained by Mingxin Liu
+//updated by Mingxin Liu
+//bright pixel detection
+// publish result of processed image on topic "/image_converter/output_video"
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+static const std::string OPENCV_WINDOW = "Open-CV display window";
+using namespace std;
+
+int g_brightratio; //threshold to decide if a pixel qualifies as dominantly "bright"
+
+class ImageConverter {
+    ros::NodeHandle nh_;
+    image_transport::ImageTransport it_;
+    image_transport::Subscriber image_sub_;
+    image_transport::Publisher image_pub_;
+
+public:
+
+    ImageConverter(ros::NodeHandle &nodehandle)
+    : it_(nh_) {
+        // Subscribe to input video feed and publish output video feed
+        image_sub_ = it_.subscribe("usb_cam/image_raw", 1,
+                &ImageConverter::imageCb, this);
+        image_pub_ = it_.advertise("/image_converter/output_video", 1);
+
+        cv::namedWindow(OPENCV_WINDOW);
+    }
+
+    ~ImageConverter() {
+        cv::destroyWindow(OPENCV_WINDOW);
+    }
+
+    //image comes in as a ROS message, but gets converted to an OpenCV type
+    void imageCb(const sensor_msgs::ImageConstPtr& msg) {
+        cv_bridge::CvImagePtr cv_ptr; //OpenCV data type
+        try {
+            cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::MONO8);//Changed RGB8 to MONO8 becuase the image is one plane.
+        } catch (cv_bridge::Exception& e) {
+            ROS_ERROR("cv_bridge exception: %s", e.what());
+            return;
+        }
+        // look for red pixels; turn all other pixels black, and turn red pixels white
+        int npix = 0; //count the red pixels
+        int isum = 0; //accumulate the column values of red pixels
+        int jsum = 0; //accumulate the row values of red pixels
+        int greyval;
+	int testval;
+        cv::Vec2b greypix; // OpenCV representation of an grey pixel
+        //comb through all pixels (j,i)= (row,col)
+        for (int i = 0; i < cv_ptr->image.cols; i++) {
+            for (int j = 0; j < cv_ptr->image.rows; j++) {
+                greypix = cv_ptr->image.at<cv::Vec2b>(j, i); //extract an RGB pixel
+                //examine intensity of R, G and B components (0 to 255)
+                greyval = greypix[0] ; 
+
+                //look for red values that are large compared to blue+green
+              
+                //if red (enough), paint this white:
+                if (greyval > g_brightratio) {
+          
+                    npix++; //note that found another bright pixel
+                    isum += i; //accumulate row and col index vals
+                    jsum += j;
+                } 
+            }
+        }
+        //cout << "npix: " << npix << endl;
+        //paint in a blue square at the centroid:
+        int half_box = 5; // choose size of box to paint
+        int i_centroid, j_centroid;
+        double x_centroid, y_centroid;
+        if (npix > 0) {
+            i_centroid = isum / npix; // average value of u component of red pixels
+            j_centroid = jsum / npix; // avg v component
+            x_centroid = ((double) isum)/((double) npix); //floating-pt version
+            y_centroid = ((double) jsum)/((double) npix);
+            ROS_INFO("u_avg: %f; v_avg: %f",x_centroid,y_centroid);
+            //cout << "i_avg: " << i_centroid << endl; //i,j centroid of red pixels
+            //cout << "j_avg: " << j_centroid << endl;
+            for (int i_box = i_centroid - half_box; i_box <= i_centroid + half_box; i_box++) {
+                for (int j_box = j_centroid - half_box; j_box <= j_centroid + half_box; j_box++) {
+                    //make sure indices fit within the image 
+                    if ((i_box >= 0)&&(j_box >= 0)&&(i_box < cv_ptr->image.cols)&&(j_box < cv_ptr->image.rows)) {
+                        cv_ptr->image.at<cv::Vec2b>(j_box, i_box)[0] = 255; //(255,0,0) is pure blue
+                        cv_ptr->image.at<cv::Vec2b>(j_box, i_box)[1] = 0;
+                        cv_ptr->image.at<cv::Vec2b>(j_box, i_box)[2] = 0;
+                    }
+                }
+            }
+
+        }
+        // Update GUI Window; this will display processed images on the open-cv viewer.
+        cv::imshow(OPENCV_WINDOW, cv_ptr->image);
+        cv::waitKey(3); //need waitKey call to update OpenCV image window
+
+        // Also, publish the processed image as a ROS message on a ROS topic
+        // can view this stream in ROS with: 
+        //rosrun image_view image_view image:=/image_converter/output_video
+        image_pub_.publish(cv_ptr->toImageMsg());
+
+    }
+}; //end of class definition
+
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "red_pixel_finder");
+    ros::NodeHandle n; //        
+    ImageConverter ic(n); // instantiate object of class ImageConverter
+    //cout << "enter red ratio threshold: (e.g. 10) ";
+    //cin >> g_brightratio;
+    g_brightratio= 125; //choose a threshold to define what is "bright" enough
+    ros::Duration timer(0.1);
+    double x, y, z;
+    while (ros::ok()) {
+        ros::spinOnce();
+        timer.sleep();
+    }
+    return 0;
+}
